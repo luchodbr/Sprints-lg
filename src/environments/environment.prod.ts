c0532aba74d28e4a41705715589f768165393308
Luciano Gil <lgil2@seidor.es>
2021-03-25 01:50:54 -0300

    Primer commit, login, register en firebase, y deploy en heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+   apiKey: "AIzaSyARwBspCGbV_XoQbSSQSS_m-9nfG1otQ0Y",
+   authDomain: "tplglab4.firebaseapp.com",
+   projectId: "tplglab4",
+   storageBucket: "tplglab4.appspot.com",
+   messagingSenderId: "90254858969",
+   appId: "1:90254858969:web:98a615099c58f5ff52fcc8"
+ }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
    firebaseConfig : {
     apiKey: "AIzaSyARwBspCGbV_XoQbSSQSS_m-9nfG1otQ0Y",
     authDomain: "tplglab4.firebaseapp.com",
