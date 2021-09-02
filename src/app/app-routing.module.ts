b456b5744b32b4b505540ffea911b79a6d384efc
Luciano Gil <luciano.gil@seidor.com>
2021-09-02 18:54:53 -0300

    sprints 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
+import { WhoimComponent } from './Components/whoim/whoim.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent},
-  {path:'register', component: RegisterComponent}
- //, {path:'**', component:ErrorCompoent}
+  {path:'register', component: RegisterComponent},
+  {path:'whoim', component:WhoimComponent }
+  //, {path:'**', component:ErrorCompoent}
 
 
 ];
